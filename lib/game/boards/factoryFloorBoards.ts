709db8ac0cb0b9a75d65d87901c2ca49310b115a
Greg Bahns <greg@bahns.com>
2025-08-17 19:29:37 -0400

    added number to TEST_BOARD starting positions

--- lib/game/boards/factoryFloorBoards.ts
+++ lib/game/boards/factoryFloorBoards.ts
@@ -14,14 +14,14 @@ export const TEST_BOARD: BoardDefinition = {
     width: 12,
     height: 12,
     startingPositions: [
-        { position: { x: 1, y: 1 }, direction: Direction.UP },
-        { position: { x: 10, y: 1 }, direction: Direction.UP },
-        { position: { x: 10, y: 10 }, direction: Direction.DOWN },
-        { position: { x: 1, y: 10 }, direction: Direction.DOWN },
-        { position: { x: 5, y: 5 }, direction: Direction.RIGHT },
-        { position: { x: 6, y: 6 }, direction: Direction.LEFT },
-        { position: { x: 3, y: 3 }, direction: Direction.DOWN },
-        { position: { x: 8, y: 8 }, direction: Direction.UP }
+        { number: 1, position: { x: 1, y: 1 }, direction: Direction.UP },
+        { number: 2, position: { x: 10, y: 1 }, direction: Direction.UP },
+        { number: 3, position: { x: 10, y: 10 }, direction: Direction.DOWN },
+        { number: 4, position: { x: 1, y: 10 }, direction: Direction.DOWN },
+        { number: 5, position: { x: 5, y: 5 }, direction: Direction.RIGHT },
+        { number: 6, position: { x: 6, y: 6 }, direction: Direction.LEFT },
+        { number: 7, position: { x: 3, y: 3 }, direction: Direction.DOWN },
+        { number: 8, position: { x: 8, y: 8 }, direction: Direction.UP }
     ],
     tiles: []
 };
